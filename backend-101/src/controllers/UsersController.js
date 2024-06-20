2a263f4003490724d2285ebb462bbf8c4c706b43
CarolFons <carolsantosfonseca@outlook.com>
2024-06-19 21:45:01 -0300

    migrations for the database

--- backend-101/src/controllers/UsersController.js
+++ backend-101/src/controllers/UsersController.js
@@ -1,4 +1,8 @@
+const {hash} = require('bcryptjs') // criptografia da senha
 const AppError = require('../utils/AppError');
+
+const sqliteConnection = require("../database/sqlite");
+
 class UsersController{
     //a classe permite que se tenha várias funções e acesse várias funções tbm
 
@@ -11,13 +15,25 @@ class UsersController{
      */
 
 //criando um usuário
-    create(request,response){
+    async create(request,response){
         const {name,email,password} = request.body;
+        //criando a conexão com o banco de dados
+        const database = await sqliteConnection();
 
-        if(!name){
-            throw new AppError("Nome é Obrigatório")
+        //verificar se o email do usuário ja existe
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]) // retorna todos os usuários do bd que possuem o email enviado
+        //validação 
+        if(checkUserExists){
+            throw new AppError("Email already registered");
         }
-        response.status(201).json({name,email,password});//receber em formato json
+        //criptografando a senha do usuário
+        const hashedPassword = await hash(password,8);
+        //inserindo os dados do usuário (criando um usuário)
+        await database.run("INSERT INTO users  (name, email, password) VALUES (?,?,?)", 
+            [name,email,hashedPassword] //enviando a senha criptografada
+        );
+
+        return response.status(201).json()
 
       
     }
